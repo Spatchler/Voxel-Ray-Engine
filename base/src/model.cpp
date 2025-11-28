10acc9acf1497174554b436c0a9f3991b6bb0c3e
spatchler <icodecubes@outlook.com>
2025-11-28 20:44:23 +0000

    Worked on model loading

--- base/src/model.cpp
+++ base/src/model.cpp
@@ -22,31 +22,35 @@ void RTVE::SVDAGModel::loadModel(const std::string& pPath) {
 
   mDirectory = pPath.substr(0, pPath.find_last_of('/'));
 
-  // processNode(scene->mRootNode, scene);
+  processNode(scene->mRootNode, scene);
 
-  mMeshVertices.push_back(glm::vec3(10, 10, 10));
-  mMeshVertices.push_back(glm::vec3(20, 10, 10));
-  mMeshVertices.push_back(glm::vec3(10, 10, 20));
-  mMeshVertices.push_back(glm::vec3(21, 10, 21));
+  // mMeshVertices.push_back(glm::vec3(10, 10, 10));
+  // mMeshVertices.push_back(glm::vec3(20, 10, 10));
+  // mMeshVertices.push_back(glm::vec3(10, 10, 20));
+  // mMeshVertices.push_back(glm::vec3(21, 10, 21));
 
-  mMeshIndices.push_back(0);
-  mMeshIndices.push_back(1);
-  mMeshIndices.push_back(2);
-
-  mMeshIndices.push_back(3);
-  mMeshIndices.push_back(1);
-  mMeshIndices.push_back(2);
+  // mMeshIndices.push_back(0);
+  // mMeshIndices.push_back(1);
+  // mMeshIndices.push_back(2);
+  
+  // mMeshIndices.push_back(3);
+  // mMeshIndices.push_back(1);
+  // mMeshIndices.push_back(2);
 
   mData.push_back({glm::vec4(0.1, 0.1, 0.1, 0)});
 
-  mVoxelCount = 0;
-  mTriangleCount = mMeshIndices.size() / 3;
+  mVoxelCount = 0u;
+  mTriangleCount = mMeshIndices.size() / 3u;
 
-  float triangleCountInv = 1.f/(float)mTriangleCount;
-  uint countSinceLastPrint = 0;
-  uint progressDelay = mTriangleCount / 2;
+  float triangleCountInv = 1.f/mTriangleCount;
+  uint countSinceLastPrint = 0u;
+  // uint progressDelay = std::max(1u, mTriangleCount / 100u);
+  uint progressDelay = mTriangleCount / 100u;
+  std::println("ProgressDelay: {}", progressDelay);
+  std::println("ProgressDelay: {}", progressDelay);
 
   for (uint i = 0; i < mTriangleCount; ++i) { // Every triangle
+    std::println("Tri: {}", i);
     // Add points to array
     std::array<glm::vec3, 3> points;
     for (uint j = 0; j < 3; ++j) {
@@ -70,10 +74,10 @@ void RTVE::SVDAGModel::loadModel(const std::string& pPath) {
       // point.z += mResolution / 2.f;
 
       // Green player
-      // point *= mResolution * 2;
-      // point.x += mResolution / 2.f;
-      // point.y += mResolution / 2.f;
-      // point.z += mResolution / 2.f;
+      point *= mResolution * 2;
+      point.x += mResolution / 2.f;
+      point.y += mResolution / 2.f;
+      point.z += mResolution / 2.f;
 
       // Dragon
       // point += 1;
@@ -109,25 +113,17 @@ void RTVE::SVDAGModel::loadModel(const std::string& pPath) {
     if (v3index(points[1], dominantAxisIndex) > v3index(points[2], dominantAxisIndex)) std::swap(points[1], points[2]);
     if (v3index(points[0], dominantAxisIndex) > v3index(points[1], dominantAxisIndex)) std::swap(points[0], points[1]);
 
-    // Add the three points of the triangle early (Not necessary)
-    // mVoxelGrid[floor(points[0].x)][floor(points[0].y)][floor(points[0].z)] = true;
-    // mVoxelGrid[floor(points[1].x)][floor(points[1].y)][floor(points[1].z)] = true;
-    // mVoxelGrid[floor(points[2].x)][floor(points[2].y)][floor(points[2].z)] = true;
-
     // Calculate direction and inverse for lines
     glm::vec3 lhDir = points[2] - points[0];
-    // lhDir = normalize(lhDir);
     glm::vec3 lhDirInv = 1.f/lhDir;
 
     glm::vec3 lmDir = points[1] - points[0];
-    // lmDir = normalize(lmDir);
     glm::vec3 lmDirInv = 1.f/lmDir;
 
     glm::vec3 mhDir = points[2] - points[1];
-    // mhDir = normalize(mhDir);
     glm::vec3 mhDirInv = 1.f/mhDir;
 
-    // Iterate over along the dominant axis traversing from between the two lines
+    // Iterate over along the dominant axis traversing between the two lines
     for (float a = v3index(points[0], dominantAxisIndex); a <= v3index(points[2], dominantAxisIndex); ++a) {
       glm::vec3* l1Dir;
       glm::vec3* l1DirInv;
@@ -149,49 +145,61 @@ void RTVE::SVDAGModel::loadModel(const std::string& pPath) {
       glm::vec2 l2Pos = glm::vec2(v3index(points[0], nonDominantAxisIndices[0]), v3index(points[0], nonDominantAxisIndices[1])) + l2t * glm::vec2(v3index(*l2Dir, nonDominantAxisIndices[0]), v3index(*l2Dir, nonDominantAxisIndices[1]));
 
       glm::vec2 dir = l2Pos - l1Pos; // Dir from l1 to l2 intersection points
-      // dir = normalize(dir);
       glm::vec2 dirInv = 1.f/dir;
 
       glm::vec2 voxelPos = glm::floor(l1Pos);
+      voxelPos.x -= glm::sign(dir.x);
+      voxelPos.y -= glm::sign(dir.y);
+
+      std::println("Dir: {}, {}, voxelPos: {}, {}, dirInvSigns: {}, {}, dirSigns: {}, {}", dir.x, dir.y, voxelPos.x, voxelPos.y, glm::sign(dirInv.x), glm::sign(dirInv.y), glm::sign(dir.x), glm::sign(dir.y));
+
+      // Draw line
       while (voxelPos != glm::floor(l2Pos)) {
+      std::println("voxelPos: {}, {}, l2pos: {}, {}, signDirInv: {}, {}", voxelPos.x, voxelPos.y, l2Pos.x, l2Pos.y, glm::sign(dirInv.x), glm::sign(dirInv.y));
+      // while ( (((voxelPos.x <= l2Pos.x) * std::max(0.f, glm::sign(dirInv.x))) ||
+      //          ((voxelPos.x >= l2Pos.x) * std::max(0.f, glm::sign(dirInv.x) * -1))) &&
+      //         (((voxelPos.y <= l2Pos.y) * std::max(0.f, glm::sign(dirInv.y))) ||
+      //          ((voxelPos.y >= l2Pos.y) * std::max(0.f, glm::sign(dirInv.y) * -1))) ) {
         float plane1 = voxelPos.x + std::max(0.f, glm::sign(dirInv.x));
         float plane2 = voxelPos.y + std::max(0.f, glm::sign(dirInv.y));
 
         if (plane1 == l2Pos.x && plane2 == l2Pos.y) { // If the destination point lies exactly on the planes
           voxelPos += glm::sign(dirInv);
-          glm::vec3 v = glm::floor(toVec3(a, voxelPos.x, voxelPos.y, dominantAxisIndex));
+          glm::vec3 v = (toVec3(a, voxelPos.x, voxelPos.y, dominantAxisIndex));
+          v = glm::floor(v);
           if (!mVoxelGrid[v.x][v.y][v.z]) {
             ++mVoxelCount;
             insert(v, {glm::vec4(1, 1, 1, 0)});
+            mVoxelGrid[v.x][v.y][v.z] = true;
           }
-          mVoxelGrid[v.x][v.y][v.z] = true;
           break;
         }
         
         float t1 = (plane1 - l1Pos.x) * dirInv.x;
         float t2 = (plane2 - l1Pos.y) * dirInv.y;
 
-        if (t1 < t2) {
+        if (t1 < t2)
           voxelPos.x += glm::sign(dirInv.x);
-        }
-        else {
+        else
           voxelPos.y += glm::sign(dirInv.y);
-        }
 
-        glm::vec3 v = glm::floor(toVec3(a, voxelPos.x, voxelPos.y, dominantAxisIndex));
+        glm::vec3 v = (toVec3(a, voxelPos.x, voxelPos.y, dominantAxisIndex));
+        v = glm::floor(v);
         if (!mVoxelGrid[v.x][v.y][v.z]) {
           ++mVoxelCount;
           insert(v, {glm::vec4(1, 1, 1, 0)});
+          mVoxelGrid[v.x][v.y][v.z] = true;
         }
-        mVoxelGrid[v.x][v.y][v.z] = true;
       }
     }
 
-    if (countSinceLastPrint > progressDelay) {
-      printProgressBar(i * triangleCountInv, "Voxelizing:");
-      countSinceLastPrint = 0;
+    if (mTriangleCount > 100) {
+      ++countSinceLastPrint;
+      if (countSinceLastPrint > progressDelay) {
+        printProgressBar((i + 1) * triangleCountInv, "Voxelizing:");
+        countSinceLastPrint = 0;
+      }
     }
-    ++countSinceLastPrint;
   }
 }
 
